312e5a0ff7c8d04d5d92d025200041a9d46f493f
wswind <18399096+wswind@users.noreply.github.com>
2020-05-08 22:20:13 +0800

    init

---
+++ GettingStarted/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace GettingStarted
+{
+  public class Message
+{ 
+    public string Text { get; set; }
+}
+
+public class Program
+{
+    public static async Task Main()
+    {
+        var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+        {
+                sbc.Host("rabbitmq://192.168.56.10",host =>
+                {
+                    host.Username("rabbit");
+                    host.Password("rabbit");
+                }
+            );
+           
+
+            sbc.ReceiveEndpoint("test_queue", ep =>
+            {
+                ep.Handler<Message>(context =>
+                {
+                    return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                });
+            });
+        });
+
+        await bus.StartAsync(); // This is important!
+        
+        await bus.Publish(new Message{Text = "Hi"});
+        
+        Console.WriteLine("Press any key to exit");
+        await Task.Run(() => Console.ReadKey());
+        
+        await bus.StopAsync();
+    }
+}
+}
